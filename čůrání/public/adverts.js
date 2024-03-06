d2420d90b645547b16345ccd9ec90b18c4529439
Daniel Culliver <daniel.culliver@gmail.com>
2024-03-06 12:57:12 +0100

    clickable ads

--- čůrání/public/adverts.js
+++ čůrání/public/adverts.js
@@ -12,6 +12,6 @@ function placeAdvert(divs, path = "../public/reklamy", max_adverts = 6) {
 
     divs.forEach((div, id) => {
         let img_id = (seed + id) % max_adverts;
-        div.innerHTML = `<img src="${path}/reklama_${img_id + 1}.${types[img_id]}">`;
+        div.innerHTML = `<a href="https://gchd-ples2024.github.io/%C4%8D%C5%AFr%C3%A1n%C3%AD"><img src="${path}/reklama_${img_id + 1}.${types[img_id]}"><\\a>`;
     });
 }
